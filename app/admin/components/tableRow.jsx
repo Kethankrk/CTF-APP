f2d3eac98a87859022c06698413239ca72b54cb6
Kethankrk <kethankrk@gmail.com>
2023-09-17 18:21:46 +0530

    made table row client side

---
+++ app/admin/components/tableRow.jsx
@@ -0,0 +1,14 @@
+"use client"
+export default function TableRow({ item }) {
+    return (
+        <tr className="border-[2px] border-dark py-5">
+            <td className="px-5 border-[2px] border-dark"><input type="checkbox" value={item.id} /></td>
+            <td className="w-1/12 pl-5">{item.id}</td>
+            <td className="w-4/12">{item.name}</td>
+            <td className="w-2/12">{item.category}</td>
+            <td className="w-2/12">{item.value}</td>
+            <td className="w-2/12">{item.type}</td>
+            <td className="w-1/12">{item.status}</td>
+        </tr>
+    )
+}
