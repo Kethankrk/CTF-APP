4d752dabec2576f90ffe0531dbbb6e6f8bedf5e2
Kethankrk <kethankrk@gmail.com>
2023-09-17 15:03:41 +0530

    reverted to default

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,15 @@
-"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Navbar from './components/nav'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='bg-light min-h-screen'>
-        <Navbar />
-        {children}
-        </div>
+          {children}
       </body>
     </html>
   )
