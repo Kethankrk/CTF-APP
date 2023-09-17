7783d687e6faaf61dd2b279d27169279566574c3
Kethankrk <kethankrk@gmail.com>
2023-09-17 18:21:16 +0530

    nav to add challenge page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,7 +1,20 @@
-export default function AdminChallenges() {
+import Link from "next/link"
+import prisma from "../helper/db"
+import TableRow from "./components/tableRow"
+import Image from "next/image"
+
+
+export default async function AdminChallenges() {
+    const data = await prisma.challenge.findMany()
     return (
         <div className="admin-challenges">
-            <div className="flex justify-center mt-28 text-white">
+            <div className="flex justify-center">
+                <Link href="/admin/add-challenges" className="flex flex-col items-center mt-20 w-fit">
+                    <Image src="/add.svg" height={40} width={40} />
+                    <p className="text-yellow text-3xl font-bold">Challenges</p>
+                </Link>
+            </div>
+            <div className="flex justify-center mt-12 text-white">
                 <table className="border-collapse w-[90%] challenge-table">
                     <tbody>
                         <tr className="border-[2px] border-dark">
@@ -13,15 +26,9 @@ export default function AdminChallenges() {
                             <th className="w-2/12 text-left">Type</th>
                             <th className="w-2/12 text-left">Status</th>
                         </tr>
-                        <tr className="border-[2px] border-dark py-5">
-                            <td className="px-5 border-[2px] border-dark"><input type="checkbox" /></td>
-                            <td className="w-1/12 pl-5">ID</td>
-                            <td className="w-4/12">Name</td>
-                            <td className="w-2/12">Category</td>
-                            <td className="w-2/12">Value</td>
-                            <td className="w-2/12">Type</td>
-                            <td className="w-1/12">Status</td>
-                        </tr>
+                        {data.map((item) => (
+                            <TableRow item={item} />
+                        ))}
                     </tbody>
                 </table>
             </div>
