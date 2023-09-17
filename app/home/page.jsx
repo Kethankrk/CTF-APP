9c902c35b95926af36d3c100ed5d3f341a2926db
Kethankrk <kethankrk@gmail.com>
2023-09-17 15:02:17 +0530

    changed home page to /home route

---
+++ app/home/page.jsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+    return (
+      <main className="flex home">
+        <div className="w-3/12 flex flex-col items-center pt-10 bg-dark">
+          <p className="text-yellow font-semibold underline">Notification</p>
+        </div>
+        <div className="w-9/12 px-16 pt-24">
+          <p>main</p>
+        </div>
+      </main>
+    )
+  }
+  
