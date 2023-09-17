a1ab636f7b3378f571981688697bbf4cd935ed7e
Kethankrk <kethankrk@gmail.com>
2023-09-17 12:24:28 +0530

    design and basic structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <main>
-      <h1>hello world</h1>
+    <main className="flex home">
+      <div className="w-3/12 flex flex-col items-center pt-10 bg-dark">
+        <p className="text-yellow font-semibold underline">Notification</p>
+      </div>
+      <div className="w-9/12 px-16 pt-24">
+        <p>main</p>
+      </div>
     </main>
   )
 }
