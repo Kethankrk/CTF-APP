4d3e6980ec73d04c2f62b12d57dd9143cc26bff0
Kethankrk <kethankrk@gmail.com>
2023-09-18 12:54:24 +0530

    changed add challenge form width

--- app/admin/add-challenges/page.jsx
+++ app/admin/add-challenges/page.jsx
@@ -4,7 +4,7 @@ export default function AddChallenge() {
     return (
         <div className="home flex flex-col items-center">
             <p className="text-white text-3xl font-bold mt-20">Create Challenge</p>
-            <div className="mt-10 min-w-[500px] text-white">
+            <div className="mt-10 min-w-[800px] text-white">
                 <div className="flex flex-col">
                     <label htmlFor="" className="font-medium">Challenge name:</label>
                     <input type="text" placeholder="Enter challenge name here." className="bg-dark py-1 px-2 rounded-md shadow-inner placeholder:text-sm" />
