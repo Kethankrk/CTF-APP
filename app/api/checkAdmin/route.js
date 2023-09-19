117cac98e3a04ecd5b9bf108778901a89aae0add
Kethankrk <kethankrk@gmail.com>
2023-09-19 20:57:42 +0530

    auth now verify db and delete old cookies

--- app/api/checkAdmin/route.js
+++ app/api/checkAdmin/route.js
@@ -1,3 +1,4 @@
+import prisma from "@/app/helper/db";
 import { verify } from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
@@ -15,6 +16,20 @@ export async function GET() {
     try {
         const data = verify(token.value, process.env.JWT_SEC)
         if (data.isAdmin) {
+            const usr = await prisma.user.findUnique({
+                where: {
+                    username: data.username
+                }
+            })
+
+            if (!usr.isAdmin) {
+                const response = NextResponse.json({
+                    admin: false
+                })
+                response.cookies.delete('krk');
+                return response;
+            }
+
             return NextResponse.json({
                 admin: true
             })
--- app/api/checkAuth/route.js
+++ app/api/checkAuth/route.js
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { verify } from "jsonwebtoken"
+import prisma from '@/app/helper/db'
 
 export async function GET(request) {
     const cookieStore = cookies()
@@ -15,8 +16,21 @@ export async function GET(request) {
     }
 
     try {
-        const usr = verify(token.value, process.env.JWT_SEC)
-        console.log(usr)
+        const data = verify(token.value, process.env.JWT_SEC)
+
+        const user = await prisma.user.findUnique({
+            where: {
+                username: data.username
+            }
+        })
+
+        if (!user) {
+            const response = NextResponse.json({
+                auth: false
+            });
+            response.cookies.delete('krk');
+            return response;
+        }
 
         return NextResponse.json({
             auth: true
